2ad93412f4a9844130a376f3758dc5a8aec74df3
Bhagya14 <45635523+Bhagya14@users.noreply.github.com>
2018-12-30 23:43:53 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_task_day3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter id:");
+            int emp_id = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("enter customer name:");
+            string emp_name = Console.ReadLine();
+
+            Console.WriteLine("enter item name:");
+            string emp_city = Console.ReadLine();
+
+            employee obj = new employee(emp_id, emp_name, emp_city);
+            Console.WriteLine(obj.pemp_id);
+            Console.WriteLine(obj.pemp_name);
+            Console.WriteLine(obj.pemp_city);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ employee.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_task_day3
+{
+    class employee
+    {
+        private int emp_id;
+        private string emp_name;
+        private string emp_city;
+
+        public employee(int emp_id, string emp_name, string emp_city)
+        {
+            this.emp_id = emp_id;
+            this.emp_name = emp_name;
+            this.emp_city = emp_city;
+        }
+        public int pemp_id
+        {
+            get
+            {
+                return this.emp_id;
+            }
+        }
+        public string pemp_name
+        {
+            get
+            {
+                return this.emp_name;
+            }
+        }
+        public string pemp_city
+        {
+            get
+            {
+                return this.emp_city;
+            }
+        }
+    }
+}
